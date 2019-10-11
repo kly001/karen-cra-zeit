8b6071bc033e9765fb6dca247b4c482992d6119f
kly001 <karenly1961@gmail.com>
2019-10-10 21:30:48 -0400

    Added Image

--- src/App.js
+++ src/App.js
@@ -14,40 +14,9 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Karen's First App Deployed with ZEIT</h1>
-      <h2>
-        Deployed with{' '}
-        <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          ZEIT Now
-        </a>
-        !
-      </h2>
-      <p>
-        <a
-          href="https://github.com/zeit/now-examples/tree/master/create-react-app-functions"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        was bootstrapped with{' '}
-        <a href="https://facebook.github.io/create-react-app/">
-          Create React App
-        </a>{' '}
-        and contains three directories, <code>/public</code> for static assets,{' '}
-        <code>/src</code> for components and content, and <code>/api</code>{' '}
-        which contains a serverless <a href="https://golang.org/">Go</a>{' '}
-        function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Go
-        </a>
-        .
-      </p>
-      <br />
+      <h1>Karen's First App Deployed with ZEIT Now!</h1>
+      <img src="https://www.animatedimages.org/data/media/105/animated-woman-image-0084.gif" alt="dancing granny" />
+    
       <h2>The date according to Go is:</h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
