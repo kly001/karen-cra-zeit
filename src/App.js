1ae26ba406f1b6a10ed9f6b040e78aa4077aed26
kly001 <karenly1961@gmail.com>
2019-10-10 21:20:50 -0400

    Changed Header

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
   }, []);
   return (
     <main>
-      <h1>Create React App + Go API</h1>
+      <h1>Karen's First App Deployed with ZEIT</h1>
       <h2>
         Deployed with{' '}
         <a
